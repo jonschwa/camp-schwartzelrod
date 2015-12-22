4e510efa90ab1b463d949e14cd8ba2abb2c816c5
jonschwa <jschwartz@bluestatedigital.com>
2015-12-22 07:07:59 +0000

    grid tinkering

--- resources/views/pages/_big-logo-splash.blade.php
+++ resources/views/pages/_big-logo-splash.blade.php
@@ -1,24 +1,27 @@
 <div id="main-page-splash">
-    <div class="container" id="invitation_container">
-        <div id="outer-invitation">
-            <div id="inner-invitation">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <img src="http://placehold.it/300x450">
-                        </div>
-                        <div class="col-sm-7">
-                            <div class="text-container">
-                                <p class="wedding-info-headline">
-                                    Jon Schwartz<br>
-                                    and<br>
-                                    Stacey Axelrod<br>
-                                <p>
+    <div id="invitation_container" class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div id="outer-invitation">
+                    <div id="inner-invitation">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="http://placehold.it/300x450">
                             </div>
-                            <p class="wedding-info">
-                                invite you to a fun-filled camp weekend<br>
-                                to celebrate their marriage<br>
-                            </p>
+                            <div class="col-md-1"></div>
+                            <div class="col-md-7">
+                                <div class="text-container">
+                                    <p class="wedding-info-headline">
+                                        Jon Schwartz<br>
+                                        and<br>
+                                        Stacey Axelrod<br>
+                                    <p>
+                                </div>
+                                <p class="wedding-info">
+                                    invite you to a fun-filled camp weekend<br>
+                                    to celebrate their marriage<br>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
