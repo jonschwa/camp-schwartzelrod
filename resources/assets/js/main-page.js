50d015a7bccc6c07ba8c7be880cf47bdf583d13e
jonschwa <jschwartz@bluestatedigital.com>
2016-01-17 20:39:30 +0000

    added auto height on page load

--- resources/assets/js/main-page.js
+++ resources/assets/js/main-page.js
@@ -1,12 +1,18 @@
 $( document ).ready(function() {
-    //var mainSplashArea = $('#main-page-splash');
-    //
-    ////get the page height
-    //var windowHeight = $(window).height();
-    //
-    ////adjust for page height
-    //if(windowHeight > mainSplashArea.height()) {
-    //    mainSplashArea.css('height', windowHeight - 60);
-    //}
+    var mainSplashArea = $('#main-page-splash');
+
+    //get the page height
+    var windowHeight = $(window).height();
+
+    //adjust for page height
+    if(windowHeight > mainSplashArea.height()) {
+        mainSplashArea.css('height', windowHeight);
+    }
+
+    //add some padding on the image
+    var imgHeight = $('#main-page-campsign').height();
+    var imgMargin = (windowHeight - imgHeight)/4;
+
+    $('#main-page-campsign').css('margin-top', imgMargin);
 
 });
--- resources/views/pages/_big-logo-splash.blade.php
+++ resources/views/pages/_big-logo-splash.blade.php
@@ -1,5 +1,6 @@
-<div class="container-fluid">
+
         <div id="main-page-splash">
+                <div class="container-fluid">
                 <img id="main-page-campsign" src="/img/camp-schwartzelrod-sign.png" />
         </div>
 </div>
