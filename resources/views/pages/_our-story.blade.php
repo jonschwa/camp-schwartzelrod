8161ab2db888fabf701b718169371d42391cd840
jonschwa <jschwartz@bluestatedigital.com>
2016-01-17 22:12:32 +0000

    layout fixes

--- resources/views/pages/_our-story.blade.php
+++ resources/views/pages/_our-story.blade.php
@@ -4,10 +4,7 @@
         <div class="row" id="our-story-section">
             <div id="desktop-story">
                 <div class="col-md-4 col-md-offset-1"></div>
-                <div class="col-md-7 our-story-text">
-                    <p class="clarendon-headline">Jon and Stacey</p>
-                    <p>It all started when Jon was intrigued by a 97% match on OkCupid and sent a message to Stacey on November 23, 2010. Jon had a crazy schedule as a meteorologist so we talked online until we could finally arrange a date on December 20th. Nobody got murdered and we both represented ourselves accurately online, so it was a major online dating success! In April 2011, shortly after a weekend getaway to a relaxing lake house, we made it official. We moved in together in August 2012 and luckily our cats Atlas and Lola tolerated each other. In late September 2012, we took an amazing 600-mile road trip together from Portland, OR, to San Francisco, CA. We figured out that we have no problem spending a week together in a small car or a tiny tent in the middle of nowhere.</p>
-                    <p>Jon proposed at Grounds for Sculpture on July 4th, 2013. He even got a stranger to photograph the moment! A few months later, Stacey got her dream job as the photographer for the ASPCA and inspired Jon to start looking for jobs in New York City. He made a complete career switch and accepted a job as a web developer for a fantastic agency. In January 2014, we moved on up to the East Side of Manhattan. We adopted our dog Escher in May 2014 and what an adventure it has been! We returned to the west coast (Colorado this time) for our first engagement-iversary in July 2014 and climbed the fourteener Mt. Bierstadt together. Since then, we have been enjoying life in the big city, working hard at careers we love, spending time with family and friends, and traveling whenever we get the chance. We took Escher on his first road trip in August 2015 and that’s when we found Camp Abnaki!</p>
+                <div class="col-md-7" class="our-story-text">
                 </div>
             </div>
             <div id="mobile-story" class="our-story-text">
