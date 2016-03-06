d23e206e1ba0c1efe072d893011c8ec78e31f209
jonschwa <jschwartz@bluestatedigital.com>
2016-03-06 18:52:55 +0000

    login and pw reset similar padding

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,18 +1,22 @@
 @extends("layout")
 
 @section("content")
-    <div class="container-fluid">
-        <div id="error-flash-container" class="alert alert-danger txt-centered" role="alert" style="display:none">
-            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-            <span id="error-flash-message" class="alert-error-text"></span>
-        </div>
-        <div class="clarendon-headline green txt-centered">Log In</div>
-        @include("users._login-form")
-        <div class="login-help">
-            <p class="highway-subhead centered">
-                Having trouble logging in?
-            </p>
-            <p>Click <a href="/password/recover">here</a> to reset your password.</p>
+    <div class="container-fluid" id="login-container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="error-flash-container" class="alert alert-danger txt-centered" role="alert" style="display:none">
+                    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                    <span id="error-flash-message" class="alert-error-text"></span>
+                </div>
+                <div class="clarendon-headline green txt-centered">Log In</div>
+                @include("users._login-form")
+                <div class="login-help">
+                    <p class="highway-subhead centered">
+                        Having trouble logging in?
+                    </p>
+                    <p>Click <a href="/password/recover">here</a> to reset your password.</p>
+                </div>
+            </div>
         </div>
     </div>
 @stop
