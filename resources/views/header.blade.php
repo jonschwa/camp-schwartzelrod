84f2e782fdc898220d177275803b52fa6765089e
jonschwa <jschwartz@bluestatedigital.com>
2016-03-07 04:14:06 +0000

    header rsvp tooltip, rsvp width

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -44,7 +44,7 @@
                         </ul>
                     @else
                         <ul class="nav navbar-nav navbar-right">
-                            <li>
+                            <li data-toggle="tooltip" data-placement="left" title="Wait! Make sure you scroll down and read through this page before RSVPing">
                                 <a class="highway-subhead nav-link" href="/#rsvp" id="nav-rsvp-link">RSVP</a>
                             </li>
                             <li>
