a364179c8cdb896caccf5c5b6ca67bdfb3f6c656
jonschwa <jschwartz@bluestatedigital.com>
2016-03-07 03:54:53 +0000

    non draggable google map

--- resources/views/pages/_vt-info.blade.php
+++ resources/views/pages/_vt-info.blade.php
@@ -3,7 +3,7 @@
         <div id="gmap-embed">
             <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
             <div id="gmap_canvas"></div>
-            <script type="text/javascript"> function init_map(){var myOptions = {zoom:12,scrollwheel: false,isDraggable : false,center:new google.maps.LatLng(44.766335,-73.30987499999998),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(44.766335, -73.30987499999998)});infowindow = new google.maps.InfoWindow({content:"<b>YMCA Camp Abnaki</b><br/>1252 Abnaki Rd<br/>05474 North Hero" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+            <script type="text/javascript"> function init_map(){var myOptions = {zoom:12,scrollwheel: false,draggable: false,center:new google.maps.LatLng(44.766335,-73.30987499999998),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(44.766335, -73.30987499999998)});infowindow = new google.maps.InfoWindow({content:"<b>YMCA Camp Abnaki</b><br/>1252 Abnaki Rd<br/>05474 North Hero" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
         </div>
         <a name="vt-info"></a>
         <div class="container-fluid info">
